4181f1142b5cae3caaad4adb5626ccb61cc5f7e3
Phuc000 <truongvinhphuc2003@gmail.com>
2024-05-05 17:30:01 +0700

    feat: login + signup

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,79 +1,138 @@
-// Login.js
-import React, { useState } from 'react';
-import './Login.css';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardGroup,
+  CCol,
+  CContainer,
+  CForm,
+  CFormInput,
+  CInputGroup,
+  CInputGroupText,
+  CRow,
+  CAlert,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilLockLocked, cilUser } from '@coreui/icons'
+import '@coreui/coreui/dist/css/coreui.min.css'
+import './Login.css'
 
-// import FetchRequest from '../../components/api/api';
 function Login() {
-  // State variables for username and password
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [showPopup, setShowPopup] = useState(false);
-  const [popupMessage, setPopupMessage] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPopup, setShowPopup] = useState(false);
+    const [popupMessage, setPopupMessage] = useState('');
 
-  // Function to handle form submission
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    FetchRequest('api/AuthenticationApi/Login', 'POST', {
-        UserName: username,
-        Password: password
-        }, successCallback, errorCallback);
-  };
+    // Function to handle form submission
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Username: ', username);
+        console.log('Password: ', password);
+        // FetchRequest('api/AuthenticationApi/Login', 'POST', {
+        //     UserName: username,
+        //     Password: password
+        //     }, successCallback, errorCallback);
+        setPopupMessage('Login successful');
+        setShowPopup(true);
+        setTimeout(() => {
+            window.location.href = '/dashboard';
+        }, 1500);
+    };
 
-  const successCallback = (data) => {
-    console.log('Success:', data);
-    localStorage.setItem('token', data.Token);
-    localStorage.setItem('username', data.UserName); // Store the username
-    setPopupMessage('Login successful');
-    setShowPopup(true);
-    // wait for 1 second before redirecting to dashboard
-    setTimeout(() => {
-      window.location.href = '/dashboard';
-    }, 1000);
-    
-    
-  }
-  
-  const errorCallback = (error) => {
-    console.error('Error:', error);
-    setPopupMessage('Login failed. Wrong password or username.');
-    setShowPopup(true);
-  }
+    const successCallback = (data) => {
+        console.log('Success:', data);
+        localStorage.setItem('token', data.Token);
+        localStorage.setItem('username', data.UserName); // Store the username
+        setPopupMessage('Login successful');
+        setShowPopup(true);
+        // wait for 1 second before redirecting to dashboard
+        setTimeout(() => {
+          window.location.href = '/dashboard';
+        }, 1000);
+        
+        
+    }
+      
+    const errorCallback = (error) => {
+        console.error('Error:', error);
+        setPopupMessage('Login failed. Wrong password or username.');
+        setShowPopup(true);
+    }
 
-  return (
-    <div className='login'>
-      {/* <LeftImage1 />
-      <LeftImage2 /> */}
-      <section>
-        <div className='form-loginbox'>
-          <div className='form-value'>
-            <form onSubmit={handleSubmit}>
-              <div className='loginTitle'>Login</div>
-              <div className='loginDesc'>Enter given account information that you <br/>created.</div>
-              <div className='inputbox'>
-                <input type = 'text' onChange={(e) => setUsername(e.target.value)} required/>
-                <label>User Name</label>
-              </div>
-              <div className='inputbox'>
-                <input type = 'password' onChange={(e) => setPassword(e.target.value)} required/>
-                <label>Password</label>
-              </div>
-              <button type='submit' className='login-btn'>Log in</button>
-              <div className='register-block'>
-                  <p>Don't have an account?&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href='/register'>Register</a></p>
-              </div>
-
-            </form>
-          </div>
+    return (
+        <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center login-content">
+          <CContainer>
+            <CRow className="justify-content-center">
+              <CCol md={8}>
+                <CCardGroup>
+                  <CCard className="p-4">
+                    <CCardBody>
+                      <CForm onSubmit={handleSubmit}>
+                        <h1>Login</h1>
+                        <p className="text-body-secondary">Sign In to your account</p>
+                        <CInputGroup className="mb-3">
+                          <CInputGroupText>
+                            <CIcon icon={cilUser} />
+                          </CInputGroupText>
+                          <CFormInput placeholder="Username" autoComplete="username" onChange={(e) => setUsername(e.target.value)} required/>
+                        </CInputGroup>
+                        <CInputGroup className="mb-4">
+                          <CInputGroupText>
+                            <CIcon icon={cilLockLocked} />
+                          </CInputGroupText>
+                          <CFormInput
+                            type="password"
+                            placeholder="Password"
+                            autoComplete="current-password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                          />
+                        </CInputGroup>
+                        <CRow>
+                          <CCol xs={6}>
+                            <CButton type='submit' color="primary" className="px-4">
+                              Login
+                            </CButton>
+                          </CCol>
+                          <CCol xs={6} className="text-right">
+                            <CButton color="link" className="px-0">
+                              Forgot password?
+                            </CButton>
+                          </CCol>
+                        </CRow>
+                      </CForm>
+                    </CCardBody>
+                  </CCard>
+                  <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
+                    <CCardBody className="text-center">
+                      <div>
+                        <h2>Sign up</h2>
+                        <p>
+                            Wellcome to our website.
+                          Don't have an account? Register now to get started.
+                        </p>
+                        <Link to="/register">
+                          <CButton color="primary" className="mt-3" active tabIndex={-1}>
+                            Register Now!
+                          </CButton>
+                        </Link>
+                      </div>
+                    </CCardBody>
+                  </CCard>
+                </CCardGroup>
+              </CCol>
+            </CRow>
+          </CContainer>
+            {showPopup && (
+                <CAlert className='popup' color='primary' dismissible visible={showPopup} onClose={() => setShowPopup(false)}>
+                    <p>{popupMessage}</p>
+                </CAlert>
+            )}
         </div>
-      </section>
-      {showPopup && (
-        <div className="popup">
-          <p>{popupMessage}</p>
-          <button onClick={() => setShowPopup(false)}>Close</button>
-        </div>
-      )}
-    </div>
-  );
+  )
 }
 
-export default Login;
+export default Login
--- client/src/pages/Register/index.jsx
+++ client/src/pages/Register/index.jsx
@@ -1,12 +1,135 @@
-import "./Register.css";
-import React from "react";
-import { Link } from "react-router-dom";
+import './Register.css'
+import '@coreui/coreui/dist/css/coreui.min.css'
+import React from 'react'
+import { useState } from 'react'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCol,
+  CContainer,
+  CForm,
+  CFormInput,
+  CInputGroup,
+  CInputGroupText,
+  CRow,
+  CAlert,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilLockLocked, cilUser } from '@coreui/icons'
 
-const Register = () => {
+function Register() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [repeatPassword, setRepeatPassword] = useState('');
+    const [showPopup, setShowPopup] = useState(false);
+    const [popupMessage, setPopupMessage] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (password !== repeatPassword) {
+            setPopupMessage('Passwords do not match');
+            setShowPopup(true);
+            return;
+        }
+        console.log('Username: ', username);
+        console.log('Email: ', email);
+        console.log('Password: ', password);
+        // FetchRequest('api/AuthenticationApi/Register', 'POST', {
+        //     Email: email,
+        //     UserName: username,
+        //     Password: password,
+            
+        // }, successCallback, errorCallback);
+        setPopupMessage('Account created successfully');
+        setShowPopup(true);
+        setTimeout(() => {
+            window.location.href = '/';
+        }, 1500);
+    };
+
+    const successCallback = (data) => {
+        console.log('Success:', data);
+        localStorage.setItem('token', data.Token);
+        localStorage.setItem('username', data.UserName); // Store the username
+        window.location.href = '/';
+      }
+  
+      const errorCallback = (error) => {
+          console.error('Error:', error);
+      }
+
     return (
-        <div>
-            <h3>Register</h3>
+        <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center register-content">
+          <CContainer>
+            <CRow className="justify-content-center">
+              <CCol md={9} lg={7} xl={6}>
+                <CCard className="mx-4">
+                  <CCardBody className="p-4">
+                    <CForm onSubmit={handleSubmit}>
+                      <h1>Register</h1>
+                      <p className="text-body-secondary">Create your account</p>
+                      <CInputGroup className="mb-3">
+                        <CInputGroupText>
+                          <CIcon icon={cilUser} />
+                        </CInputGroupText>
+                        <CFormInput 
+                            placeholder="Username" 
+                            autoComplete="username" 
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                      </CInputGroup>
+                      <CInputGroup className="mb-3">
+                        <CInputGroupText>@</CInputGroupText>
+                        <CFormInput 
+                            placeholder="Email" 
+                            autoComplete="email" 
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                      </CInputGroup>
+                      <CInputGroup className="mb-3">
+                        <CInputGroupText>
+                          <CIcon icon={cilLockLocked} />
+                        </CInputGroupText>
+                        <CFormInput
+                            type="password"
+                            placeholder="Password"
+                            autoComplete="new-password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                      </CInputGroup>
+                      <CInputGroup className="mb-4">
+                        <CInputGroupText>
+                          <CIcon icon={cilLockLocked} />
+                        </CInputGroupText>
+                        <CFormInput
+                            type="password"
+                            placeholder="Repeat password"
+                            autoComplete="new-password"
+                            onChange={(e) => setRepeatPassword(e.target.value)}
+                            required
+                        />
+                      </CInputGroup>
+                      <div className="d-grid">
+                        <CButton type='submit' color="primary">Create Account</CButton>
+                      </div>
+                    </CForm>
+                  </CCardBody>
+                </CCard>
+              </CCol>
+            </CRow>
+          </CContainer>
+            {showPopup && (
+                <CAlert className='popup' color='primary' dismissible visible={showPopup} onClose={() => setShowPopup(false)}>
+                    <p>{popupMessage}</p>
+                </CAlert>
+            )}
         </div>
-    );
+    )
 }
-export default Register;
+
+export default Register
