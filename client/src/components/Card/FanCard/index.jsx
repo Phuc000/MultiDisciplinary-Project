0203ce55817468f5cfea1042276af83b4c7cb29a
Huy11042003 <coipham1104@gmail.com>
2024-05-14 00:14:40 +0700

    add mqtt connection

--- client/src/components/Card/FanCard/index.jsx
+++ client/src/components/Card/FanCard/index.jsx
@@ -1,21 +1,69 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { CFormRange } from '@coreui/react';
 import { CCollapse, CButton, CCard, CCardBody } from '@coreui/react';
 import './FanCard.css';
+import MQTT from 'mqtt';
 
 const FanCard = () => {
     const [isOn, setIsOn] = useState(false);
-    const [speed, setSpeed] = useState(30);
+    const [speed, setSpeed] = useState(40);
     const [visible, setVisible] = useState(false);
+    const [client, setClient] = useState(null);
+    const topics = ["/feeds/button2"];
+
+    useEffect(() => {
+        const connectMQTT = () => {
+            const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
+            const connectUrl = `wss://io.adafruit.com:443/mqtt}`;
+            const client = MQTT.connect(connectUrl, {
+                clientId,
+                clean: true,
+                connectTimeout: 4000,
+                username: 'CSE_MultiProject',
+                password: '',
+                reconnectPeriod: 1000,
+            });
+
+            client.on('connect', () => {
+                console.log('Connected to Adafruit IO');
+                topics.forEach(topic => {
+                    client.subscribe(`CSE_MultiProject${topic}`, () => {
+                        console.log(`Subscribed to ${topic}`);
+                    });
+                });
+            });
+
+            client.on('message', (topic, message) => {
+                console.log(`Received message: ${message.toString()}`);
+            });
+
+            client.on('error', (err) => {
+                console.error('Connection error: ', err);
+                client.end();
+            });
+
+            setClient(client);
+        };
+
+        connectMQTT();
+
+        return () => {
+            client && client.end();
+        };
+    }, []);
+
+
 
     const toggleFan = () => {
       setIsOn(!isOn);
       setVisible(false);
+      client && client.publish(`CSE_MultiProject/feeds/button2`, isOn ? '0' : '40');
     };
 
     const handleSpeedChange = (event) => {
         setSpeed(parseInt(event.target.value)); // Parse speed value as integer
+        client && client.publish(`CSE_MultiProject/feeds/button2`, event.target.value.toString());
       };
 
     return (
@@ -49,7 +97,7 @@ const FanCard = () => {
                         <CFormRange
                           min="0"
                           max="100"
-                          step="1"
+                          step="4"
                           value={speed}
                           onChange={handleSpeedChange}/>
                         <p>Speed: {speed}</p>
--- client/src/components/Card/LightCard/index.jsx
+++ client/src/components/Card/LightCard/index.jsx
@@ -1,12 +1,56 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import './LightCard.css';
+import MQTT from 'mqtt';
 
 const LightCard = ({ light }) => {
     const [isOn, setIsOn] = useState(false);
+    const [client, setClient] = useState(null);
+    const topics = ["/feeds/button1"];
 
+    useEffect(() => {
+        const connectMQTT = () => {
+            const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
+            const connectUrl = `wss://io.adafruit.com:443/mqtt}`;
+            const client = MQTT.connect(connectUrl, {
+                clientId,
+                clean: true,
+                connectTimeout: 4000,
+                username: 'CSE_MultiProject',
+                password: '',
+                reconnectPeriod: 1000,
+            });
+
+            client.on('connect', () => {
+                console.log('Connected to Adafruit IO');
+                topics.forEach(topic => {
+                    client.subscribe(`CSE_MultiProject${topic}`, () => {
+                        console.log(`Subscribed to ${topic}`);
+                    });
+                });
+            });
+
+            client.on('message', (topic, message) => {
+                console.log(`Received message: ${message.toString()}`);
+            });
+
+            client.on('error', (err) => {
+                console.error('Connection error: ', err);
+                client.end();
+            });
+
+            setClient(client);
+        };
+
+        connectMQTT();
+
+        return () => {
+            client && client.end();
+        };
+    }, []);
     const toggleLight = () => {
       setIsOn(!isOn);
+      client && client.publish(`CSE_MultiProject/feeds/button1`, isOn ? '0' : '1');
     };
 
     return (
